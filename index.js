c8e2199bdba85dad2ec686ea5791016e17a6e1a5
Codenope243 <homerunn1571977@gmail.com>
2024-04-10 21:06:45 +0200

    produkt bild auswahl implementirt und produkt menge so angepast das es nicht unter 1 geht

--- index.js
+++ index.js
@@ -1,223 +1,223 @@
 const bibliothek={
     Eisenbarren:{
-        bild:"Eisenbarren.png",
+        bild:"url('bilder/Eisenbarren.png')",
         produktionszeit:2,
         ressurce1:1,
         ertrag:1,
     },
 
     Eisenstangen:{
-        bild:"Eisenbarren.png",
+        bild:"url('bilder/Eisenstangen.png')",
         produktionszeit:14,
         ressurce1:1,
         ertrag:1,
     },
 
     Eisenplatten:{
-        bild:"Eisenbarren.png",
+        bild:"url('bilder/Eisenplatten.png')",
         produktionszeit:6,
         ressurce1:3,
         ertrag:2,
     },
 
     Kupferbarren:{
-        bild:"Eisenbarren.png",
+        bild:"url('bilder/Kupferbarren.png')",
         produktionszeit:2,
         ressurce1:1,
         ertrag:1,
     },
 
     Kupferdraht:{
-        bild:"Eisenbarren.png",
+        bild:"url('bilder/Kupferdraht.png')",
         produktionszeit:4,
         ressurce1:1,
         ertrag:2,
     },
 
     Kabel:{
-        bild:"Eisenbarren.png",
+        bild:"url('bilder/Kabel.png')",
         produktionszeit:2,
         ressurce1:2,
         ertrag:1,
     },
 
     Beton:{
-        bild:"Eisenbarren.png",
+        bild:"url('bilder/Beton.png')",
         produktionszeit:4,
         ressurce1:3,
         ertrag:1,
     },
 
     Schrauben:{
-        bild:"Eisenbarren.png",
+        bild:"url('bilder/Schrauben.png')",
         produktionszeit:6,
         ressurce1:1,
         ertrag:4,
     },
 
     VerstärkteEisenplatten:{
-        bild:"Eisenbarren.png",
+        bild:"url('bilder/Verstärkte Eisenplatten.png')",
         produktionszeit:12,
         ressurce1:6,
-        ressurce2ressurce2:12,
+        ressurce2r:12,
         ertrag:1,
     },
 
     Fabpatrronen:{
-        bild:"Eisenbarren.png",
+        bild:"url('bilder/Fabpatrronen.png')",
         produktionszeit:6,
         ressurce1:5,
         ertrag:10,
     },
 
     Kupferblech:{
-        bild:"Eisenbarren.png",
+        bild:"url('bilder/Kupferblech.png')",
         produktionszeit:6,
         ressurce1:2,
         ertrag:1,
     },
 
     Rotoren:{
-        bild:"Eisenbarren.png",
+        bild:"url('bilder/Rotoren.png')",
         produktionszeit:15,
         ressurce1:5,
-        ressurce2ressurce2:25,
+        ressurce2r:25,
         ertrag:1,
     },
 
     modularerRahmen:{
-        bild:"Eisenbarren.png",
+        bild:"url('bilder/modularer Rahmen.png')",
         produktionszeit:60,
         ressurce1:3,
-        ressurce2ressurce2:12,
+        ressurce2r:12,
         ertrag:2,
     },
 
     IntelligenteBeschichtung:{
-        bild:"Eisenbarren.png",
+        bild:"url('bilder/Intelligente Beschichtung.png')",
         produktionszeit:30,
         ressurce1:1,
-        ressurce2ressurce2:1,
+        ressurce2r:1,
         ertrag:1,
     },
 
     AlienDNAKapsel:{
-        bild:"Eisenbarren.png",
+        bild:"url('bilder/Alien DNA-Kapsel.png')",
         produktionszeit:6,
         ressurce1:1,
         ertrag:1,
     },
 
     Stahlbarren:{
-        bild:"Eisenbarren.png",
+        bild:"url('bilder/Stahlbarren.png')",
         produktionszeit:4,
         ressurce1:3,
-        ressurce2ressurce2:3,
+        ressurce2r:3,
         ertrag:3,
     },
 
     Stahlträger:{
-        bild:"Eisenbarren.png",
+        bild:"url('bilder/Stahlträger.png')",
         produktionszeit:4,
         ressurce1:4,
         ertrag:1,
     },
 
     Stahlrohre:{
-        bild:"Eisenbarren.png",
+        bild:"url('bilder/Stahlrohre.png')",
         produktionszeit:6,
         ressurce1:3,
         ertrag:2, 
     },
 
     Mehrzweckgerüst:{
-        bild:"Eisenbarren.png",
+        bild:"url('bilder/Mehrzweckgerüst.png')",
         produktionszeit:24,
         ressurce1:1,
-        ressurce2ressurce2:12,
+        ressurce2r:12,
         ertrag:2,
     },
 
     Stahlbetontrager:{
-        bild:"Eisenbarren.png",
+        bild:"url('bilder/Stahlbetontrager.png')",
         produktionszeit:10,
         ressurce1:4,
-        ressurce2ressurce2:5,
+        ressurce2r:5,
         ertrag:1,
     },
 
     Stator:{
-        bild:"Eisenbarren.png",
+        bild:"url('bilder/Stator.png')",
         produktionszeit:12,
         ressurce1:3,
-        ressurce2ressurce2:8,
+        ressurce2r:8,
         ertrag:1,
     },
 
     Motor:{
-        bild:"Eisenbarren.png",
+        bild:"url('bilder/Motor.png')",
         produktionszeit:12,
         ressurce1:2,
-        ressurce2ressurce2:2,
+        ressurce2r:2,
         ertrag:1,
     },
 
     AutomatischeVerkabelung:{
-        bild:"Eisenbarren.png",
+        bild:"url('bilder/Automatische Verkabelung.png')",
         produktionszeit:24,
         ressurce1:1,
-        ressurce2ressurce2:20,
+        ressurce2r:20,
         ertrag:1,
     },
 
     SchwerermodularerRahmen:{
-        bild:"Eisenbarren.png",
+        bild:"url('bilder/Schwerer modularer Rahmen.png')",
         produktionszeit:30,
         ressurce1:5,
-        ressurce2ressurce2:15,
+        ressurce2r:15,
         ressurce3:5,
         ressurce4:100,
         ertrag:1,
     },
 
     Cateriumbarren:{
-        bild:"Eisenbarren.png",
+        bild:"url('bilder/Cateriumbarren.png')",
         produktionszeit:4,
         ressurce1:3,
         ertrag:1,
     },
 
     Turbodraht:{
-        bild:"Eisenbarren.png",
+        bild:"url('bilder/Turbodraht.png')",
         produktionszeit:5,
         ressurce1:1,
         ertrag:5,
     },
 
     KIBegrenzer:{
-        bild:"Eisenbarren.png",
+        bild:"url('bilder/KI-Begrenzer.png')",
         produktionszeit:12,
         ressurce1:5,
-        ressurce2ressurce2:20,
+        ressurce2r:20,
         ertrag:1,
     },
 
     Quarzkristall:{
-        bild:"Eisenbarren.png",
+        bild:"url('bilder/Quarzkristall.png')",
         produktionszeit:8,
         ressurce1:5,
         ertrag:3,
     },
 
     Quarzsand:{
-        bild:"Eisenbarren.png",
+        bild:"url('bilder/Quarzsand.png')",
         produktionszeit:8,
         ressurce1:3,
         ertrag:5,
     },
 //nebenprudukt von kunstof (schweröllrükstand)(1)muss noch hinzugefügt werdern
     Kunststoff:{
-        bild:"Eisenbarren.png",
+        bild:"url('bilder/Kunststoff.png')",
         produktionszeit:6,
         ressurce1:3,
         ertrag:2,
@@ -225,85 +225,91 @@ const bibliothek={
     },
 //nebenprudukt von Gummi (schweröllrükstand)(2)muss noch hinzugefügt werdern
     Gummi:{
-        bild:"Eisenbarren.png",
+        bild:"url('bilder/Gummi.png')",
         produktionszeit:6,
         ressurce1:3,
         ertrag:2,
     },
 
     Platine:{
-        bild:"Eisenbarren.png",
+        bild:"url('bilder/Platine.png')",
         produktionszeit:8,
         ressurce1:2,
-        ressurce2ressurce2:4,
+        ressurce2r:4,
         ertrag:1,
     },
 
     Computer:{
-        bild:"Eisenbarren.png",
+        bild:"url('bilder/Computer.png')",
         produktionszeit:24,
         ressurce1:4,
-        ressurce2ressurce2:4,
+        ressurce2r:4,
         ressurce3:4,
         ressurce4:4,
         ertrag:1,
     },
 
     ModularerMotor:{
-        bild:"Eisenbarren.png",
+        bild:"url('bilder/Modularer Motor.png')",
         produktionszeit:60,
         ressurce1:2,
-        ressurce2ressurce2:15,
+        ressurce2r:15,
         ressurce3:2,
         ertrag:1,
     },
 
     AdaptiveSteuereinheit:{
-        bild:"Eisenbarren.png",
+        bild:"url('bilder/Adaptive Steuereinheit.png')",
         produktionszeit:120,
         ressurce1:15,
-        ressurce2ressurce2:10,
+        ressurce2r:10,
         ressurce3:2,
         ressurce4:2,
         ertrag:2,
     },
 
     LeererKanister:{
-        bild:"Eisenbarren.png",
+        bild:"url('bilder/Leerer Kanister.png')",
         produktionszeit:4,
         ressurce1:2,
         ertrag:4,
     },
 
     HighspeedAnschluss:{
-        bild:"Eisenbarren.png",
+        bild:"url('bilder/Highspeed-Anschluss.png')",
         produktionszeit:16,
         ressurce1:56,
-        ressurce2ressurce2:10,
+        ressurce2:10,
         ressurce3:1,
         ertrag:1,
     },
 
     Quarzoszillator:{
-        bild:"Eisenbarren.png",
+        bild:"url('bilder/Quarzoszillator.png')",
         produktionszeit:120,
         ressurce1:36,
-        ressurce2ressurce2:28,
+        ressurce2r:28,
         ressurce3:5,
         ertrag:2,
     },
 }
 
 let produkt = ""
+produkt="Eisenbarren"
 
 setInterval(function() {
     let produktionsmengeInput = document.getElementsByClassName("produkt-menge-feld")[0];
     let produktionsmenge = parseFloat(produktionsmengeInput.value);
-    
-    if (produktionsmenge < 0) {
-        produktionsmengeInput.value = 0;
+
+    if (produktionsmenge < 1) {
+        produktionsmengeInput.value =1;
     }
 
+    if(isNaN(produktionsmenge)){
+        produktionsmengeInput.value =1;
+    }else(
+        console.log("haus macht bummmmm")
+    )
 
     const werte = bibliothek[produkt]
 
@@ -323,20 +329,25 @@ setInterval(function() {
             const benötigteRessurce2  = produktionsmenge * ressurce2 / ertrag;
 
 
-            const ressourcenProMinFeld = document.getElementsByClassName("produkt-menge-feld")[0];
+            const ressourcenProMinFeld = document.getElementsByClassName("ressursen-menge")[0];
             ressourcenProMinFeld.innerHTML = "Pro min:" + benötigteRessurce1;
-            
+            console.log(benötigteRessurce1)
+
             const produktbildContainer = document.querySelector(".produktbild-container");
-            produktbildContainer.style.backgroundImage = "url(hintergrund-bilder/platzhalter.jpg)";
-            
+            produktbildContainer.style.backgroundImage = bild;
+           
 
     }else{
         console.log("Produkt nicht gefunden in der Bibliothek");
     }
 
 
+
+
     
 }, 100);
+
+
 
 
 function produktmengeVerringern(){
@@ -345,6 +356,7 @@ function produktmengeVerringern(){
     produktmengeInput.value--
 }
 
+produktmengeErhöhen()
 function produktmengeErhöhen(){
     let produktmengeInput = document.getElementsByClassName("produkt-menge-feld")[0]
 
